b23ccbbc9d028b7a9eb8cc29dcda47d008b75b44
ws <qodnjstjr@naver.com>
2022-05-01 02:32:48 +0900

    로또번호 추가

--- lottogame/src/main/java/Main.java
+++ lottogame/src/main/java/Main.java
@@ -1,5 +1,13 @@
+import lotto.Lotto;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 
+        Lotto lotto = new Lotto();
+        lotto.lottoStart(5);
     }
 }
--- lottogame/src/main/java/lotto/Lotto.java
+++ lottogame/src/main/java/lotto/Lotto.java
@@ -1,35 +1,40 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Lotto {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
     public static final int GAME_FEE = 1000;
-    private int lottoNumber[] = new int[6];
-    private String rank;
-    private int gameCount;
 
-    public int[] getLottoNumber() {
+    private final int lottoCount = 6;
+    private int random;
+    private Set<Integer> lottoNumber;
+    private String rank;
+
+    public Lotto() {
+        this.random = (int) (Math.random() * MAX_NUMBER) + MIN_NUMBER;
+    }
+
+    public void lottoStart(int gameCount) {
+        for (int i = 0; i < gameCount; i++) {
+            System.out.println(lottoMachine(gameCount));
+        }
+    }
+
+    private Set<Integer> lottoMachine(int gameCount) {
+        for (int i = 0; i < gameCount; i++) {
+            lottoNumber = new HashSet<>();
+            lottoNumber = randomNumber();
+        }
         return lottoNumber;
     }
 
-    public void setLottoNumber(int[] lottoNumber) {
-        this.lottoNumber = lottoNumber;
+    private Set<Integer> randomNumber() {
+        while (lottoNumber.size() < lottoCount) {
+            lottoNumber.add(new Lotto().random);
+        }
+        return lottoNumber;
     }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public int getGameCount() {
-        return gameCount;
-    }
-
-    public void setGameCount(int gameCount) {
-        this.gameCount = gameCount;
-    }
-
 }
